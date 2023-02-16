82317b0ed79495c411676df2f99767c1c9ec0b2f
AnnerleViloan <annerle.enderez@gmail.com>
2023-02-16 14:48:03 +0100

    Created a class called Calculate and a Method Addition and calling it from the Main program

---
+++ GitCheckpoint1/Calculate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCheckpoint1
+{
+    internal class Calculate
+    {
+        public static void Addition(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            Console.WriteLine(sum);
+        }
+    }
+}
+
